dab42fe32d9154c102d0ddb0916d5c9b2d8b38da
fnameMC <ftorresrios50@gmail.com>
2026-06-12 23:15:33 -0600

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// CONFIGURACIÓN GLOBAL DE SUPABASE
-const SUPABASE_URL = "https://ckpaxfxohrcddxmsgsuh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_hC1mHOXhTvhqzc4vy8QTgw_StFjxTXm";
-
-// Instancia global del cliente de Supabase
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
